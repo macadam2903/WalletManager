c2ce128c1dfd5b4d8bb87cf4f3eb67923081bd8a
macadam2903 <macadammaci@gmail.com>
2026-03-07 15:15:09 +0100

    Initial Commit

---
+++ Misa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Misa
+//
+//  Created by Macza Ádám on 2026. 03. 07..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Misa/MisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisaApp.swift
+//  Misa
+//
+//  Created by Macza Ádám on 2026. 03. 07..
+//
+
+import SwiftUI
+
+@main
+struct MisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
