978325b33a730a75947e1d583ec6300cc335cc48
macadam2903 <macadammaci@gmail.com>
2026-03-07 23:47:56 +0100

    v0.4

--- Misa/Utilities/Theme.swift
+++ Misa/Utilities/Theme.swift
@@ -32,12 +32,30 @@ enum PocketColorPalette {
 
     static let options: [PocketColorOption] = [
         PocketColorOption(name: "Blue", hex: "#0A84FF"),
+        PocketColorOption(name: "Royal Blue", hex: "#4169E1"),
+        PocketColorOption(name: "Navy", hex: "#1D3557"),
+        PocketColorOption(name: "Sky", hex: "#87CEEB"),
+        PocketColorOption(name: "Cyan", hex: "#00BCD4"),
         PocketColorOption(name: "Green", hex: "#30D158"),
+        PocketColorOption(name: "Lime", hex: "#A4C639"),
+        PocketColorOption(name: "Forest", hex: "#228B22"),
         PocketColorOption(name: "Orange", hex: "#FF9F0A"),
+        PocketColorOption(name: "Amber", hex: "#FFBF00"),
+        PocketColorOption(name: "Coral", hex: "#FF7F50"),
+        PocketColorOption(name: "Red", hex: "#FF3B30"),
+        PocketColorOption(name: "Burgundy", hex: "#800020"),
         PocketColorOption(name: "Pink", hex: "#FF375F"),
+        PocketColorOption(name: "Rose", hex: "#E91E63"),
+        PocketColorOption(name: "Fuchsia", hex: "#FF00FF"),
+        PocketColorOption(name: "Purple", hex: "#AF52DE"),
         PocketColorOption(name: "Teal", hex: "#64D2FF"),
         PocketColorOption(name: "Mint", hex: "#66D4CF"),
         PocketColorOption(name: "Indigo", hex: "#5E5CE6"),
-        PocketColorOption(name: "Yellow", hex: "#FFD60A")
+        PocketColorOption(name: "Yellow", hex: "#FFD60A"),
+        PocketColorOption(name: "Gold", hex: "#D4AF37"),
+        PocketColorOption(name: "Brown", hex: "#8B5E3C"),
+        PocketColorOption(name: "Slate", hex: "#708090"),
+        PocketColorOption(name: "Gray", hex: "#8E8E93"),
+        PocketColorOption(name: "Black", hex: "#1C1C1E")
     ]
 }
